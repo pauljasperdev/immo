b1549b2f46bc3849b74cc6184d10b40c0203d553
pauljaspersahr <pauljasper.sahr@gmail.com>
2025-08-16 12:14:30 +0200

    infra: add router

---
+++ infra/router.ts
@@ -0,0 +1,18 @@
+// biome-ignore-all lint: SST is globally available
+
+import { secrets } from './secrets';
+
+export const domainName =
+  $app.stage === 'production' ? '' : 'immo.pauljasper.dev';
+
+export const domainAliases =
+  $app.stage === 'production' ? [] : [`*.${$app.stage}.immo.pauljasper.dev`];
+
+export const router = new sst.aws.Router('Router', {
+  domain: {
+    name: domainName,
+    aliases: domainAliases,
+    dns: false,
+    cert: secrets.CertArn.value,
+  },
+});
